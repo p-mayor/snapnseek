42470874e156324fb41dad3ad83767d9b95e2cfa
Peter M <petem14@gmail.com>
2019-03-27 11:52:23 -0400

    fix login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -27,11 +27,21 @@ class LoginForm extends Component {
           <Form onSubmit={this.handleLogin}>
             <Form.Group controlId="formBasicUsername">
               <Form.Label>Username</Form.Label>
-              <Form.Control type="username" placeholder="Enter username" />
+              <Form.Control
+                type="username"
+                name="username"
+                placeholder="Enter username"
+                onChange={this.handleChange}
+              />
             </Form.Group>
             <Form.Group controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={this.handleChange}
+              />
             </Form.Group>
             <Button variant="primary" type="submit">
               Submit
