0572a511608dc5a8e815a0040195ac6ef4e0e5f9
KashF <Kashfarhadi@gmail.com>
2019-03-27 15:30:20 -0400

    added register function and register actions and reducers. Currently unable to register

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
-import { LoginForm, UserProfile } from ".";
+import { LoginForm, UserProfile, RegisterForm } from ".";
 
 class App extends Component {
   render() {
     return (
-      <Switch>
-        <Route exact path="/" render={() => <LoginForm />} />
-        <Route exact path="/profile" render={() => <UserProfile />} />
-      </Switch>
+      <div>
+        <Switch>
+          <Route exact path="/" render={() => <LoginForm />} />
+          <Route exact path="/profile" render={() => <UserProfile />} />
+        </Switch>
+        <Switch>
+          <Route exact path="/" render={() => <RegisterForm />} />
+        </Switch>
+      </div>
     );
   }
 }
