4b9476689a0ad6f5a3db07f4d5e0da6629d99c17
Peter M <petem14@gmail.com>
2019-04-11 11:37:57 -0400

    update links

--- src/components/ProfileGuessItem.js
+++ src/components/ProfileGuessItem.js
@@ -9,7 +9,7 @@ export class ProfileGuessItem extends Component {
   render() {
     return (
       <Feed className="feedstyle">
-        <Link to={`/targets/${this.props.target.targetId}`}>
+        <Link to={`/targets/${this.props.target.id}`}>
           <Feed.Event style={{ paddingBottom: "20px" }}>
             <Feed.Content>
               <Feed.Summary>
